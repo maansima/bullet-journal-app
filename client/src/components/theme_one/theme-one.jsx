a7b60b98b449d187bbf54d988dd9f1ee83460786
Viviana <viviana.rosas1999@gmail.com>
2018-07-17 17:05:43 +0100

    hello

---
+++ client/src/components/theme_one/theme-one.jsx
@@ -0,0 +1,41 @@
+import * as React from "react"
+
+class Theme_One extends React.Component {
+  render() {
+    return (
+      <div className="Main-app">
+        Main background and stuff
+        
+
+        <div className="sidebar-one"> 
+        sidebar two
+         <form className="input-task"> 
+      
+<input type="text" plaecholder="enter your new task here!">
+ </input>
+     
+
+    
+
+
+             
+             </form> 
+        </div>
+
+      <div className="sidebar-two"> 
+        sidebar two 
+      </div> 
+
+      <div className="sidebar-three"> 
+        sidebar three 
+      </div> 
+
+      </div>
+
+    
+      
+    )
+  }
+}
+
+export default Theme_One
