9f3fd45515ac04a45b9501c80223304d261f29ec
Viviana <viviana.rosas1999@gmail.com>
2018-07-17 19:07:54 +0100

    maansi made a change to app.tsx

--- client/src/components/theme_one/theme-one.jsx
+++ client/src/components/theme_one/theme-one.jsx
@@ -4,36 +4,15 @@ class Theme_One extends React.Component {
   render() {
     return (
       <div className="Main-app">
-        Main background and stuff
-        
-
-        <div className="sidebar-one"> 
-        sidebar two
-         <form className="input-task"> 
-      
-<input type="text" plaecholder="enter your new task here!">
- </input>
-     
-
-    
-
-
-             
-             </form> 
-        </div>
-
-      <div className="sidebar-two"> 
-        sidebar two 
-      </div> 
-
-      <div className="sidebar-three"> 
-        sidebar three 
-      </div> 
-
+        MAIN BACKGROUND APP
+        {/* this will be white with the bullet and the little input form  */}
+        <form className="input-task">
+          <input type="text" plaecholder="enter your new task here!" />
+        </form>
+        <div className="sidebar-one">SIDEBAR TWO</div>
+        <div className="sidebar-two">sidebar two</div>
+        <div className="sidebar-three">sidebar three</div>
       </div>
-
-    
-      
     )
   }
 }
