ae030a9f94a53be8296183a1ea086b91793250ea
Maansi Manchanda <maansima@usc.edu>
2018-07-17 19:05:29 +0100

    login and signup working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { hot } from "react-hot-loader"
 import { Switch, Route } from "react-router-dom"
 import HomePage from "./components/home-page/home-page"
+import LoginPage from "./components/login-page/login-page"
 import "./App.css"
 
 class App extends React.Component {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <div className="App">
         <Switch>
           <Route exact={true} path="/" component={HomePage} />
+          <Route exact={true} path="/login" component={LoginPage} />
         </Switch>
       </div>
     )
