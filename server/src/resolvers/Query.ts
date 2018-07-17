65bed5848d31c8a6e9169faf3fb6cc53ba8345d0
Bea Litonjua <bealitonjua9@gmail.com>
2018-07-17 13:47:06 +0100

    updated data model and new mutation file

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -5,4 +5,12 @@ export default {
     const id = getUserId(ctx)
     return ctx.db.query.user({ where: { id } }, info)
   },
+  async taskFeed(parent, args, ctx: Context, info) {
+    return await ctx.db.query.tasks(
+      {
+        orderBy: args.orderBy
+      },
+      info
+    )
+  }
 }
