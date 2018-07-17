32d6a3c78bc1eead3341664b874ed608b6174795
Legina Chen <leginachen@berkeley.edu>
2018-07-17 17:49:43 +0100

    login signup updates

--- client/src/components/login-page/login-page.jsx
+++ client/src/components/login-page/login-page.jsx
@@ -1,1 +1,70 @@
 import "./login-page/login-page.css"
+
+class LoginPage extends React.Component {
+    state = {
+        email: "",
+        password: ""
+    }
+
+    render() {
+        return (
+            <div>
+                <Navigation history={this.props.history} />
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt={"logo"} />
+                    <h1 className="App-title">TWEETER</h1>
+                <div className="login-form">
+                    <Mutation mutation={LOGIN}>
+                        {login => {
+                            return (
+                                <form
+                                    onSubmit={async e => {
+                                        e.preventDefault()
+                                        //WHATEVER YOU DO HERE, is run when you submit the form
+                                        const { data } = await login({
+                                            variables: {
+                                                email: this.state.email,
+                                                password: this.state.password
+                                            }
+                                        })
+                                        localStorage.setItem("token", data.login.token)
+                                        this.props.history.push("/")
+                                    }}
+                                >
+                                    <div class="boxes">
+                                        <div>
+                                            <input
+                                            type="text"
+                                            onChange={e => {
+                                                this.setState({
+                                                    email: e.target.value
+                                                })
+                                            }}
+                                            placeholder="email"
+                                        />
+                                        </div>
+                                        <div>
+                                        <input
+                                            type="password"
+                                            onChange={e => {
+                                                this.setState({
+                                                    password: e.target.value
+                                                })
+                                            }}
+                                            placeholder="password"
+                                        />
+                                        </div>
+                                    </div>
+                                    <button type="submit">LOGIN </button>
+                                </form>
+                            )
+                        }}
+                    </Mutation>
+                </div>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default LoginPage
--- client/src/components/signup-page/signup-page.jsx
+++ client/src/components/signup-page/signup-page.jsx
@@ -1,1 +1,74 @@
 import "./signup-page/signup-page.css"
+import * as React from "react"
+import { Mutation } from "react-apollo"
+// import gql from "graphql-tag"
+
+class SignUp extends React.Component {
+    state = {
+        email: "",
+        name: "",
+        password: ""
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <Navigation history={this.props.history} />
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt={"logo"} />
+                    <h1 className="App-title">TWEETER</h1>
+                <Mutation mutation={SIGNUP}>
+                    {signup => {
+                        return (
+                            <div className="login-form">
+                                <form
+                                    onSubmit={async e => {
+                                        e.preventDefault()
+                                        try {
+                                            const { data } = await signup({
+                                                variables: {
+                                                    email: this.state.email,
+                                                    name: this.state.name,
+                                                    password: this.state.password
+                                                }
+                                            })
+                                            localStorage.setItem("token", data.signup.token)
+                                            this.props.history.push(`/myprofile`)
+                                        } catch (error) {
+                                            localStorage.removeItem("token")
+                                        }
+                                    }}
+                                >
+                                    <div>
+                                    <input
+                                        placeholder="email"
+                                        onChange={e => this.setState({ email: e.target.value })}
+                                    />
+                                    </div>
+                                    <div>
+                                    <input
+                                        type="password"
+                                        placeholder="password"
+                                        onChange={e => this.setState({ password: e.target.value })}
+                                    />
+                                    </div>
+                                    <div>
+                                    <input
+                                        placeholder="name"
+                                        onChange={e => this.setState({ name: e.target.value })}
+                                    />
+                                    </div>
+                                    <button type="submit">SIGNUP!</button>
+                                </form>
+                            </div>
+                        )
+                    }}
+                </Mutation>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default SignUp
