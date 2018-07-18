4e1ffe061a80f426cd54d2adb14e357aa201c3cd
Maansi Manchanda <maansima@usc.edu>
2018-07-18 22:13:34 +0100

    ocmmit

--- client/src/components/login-page/login-page.jsx
+++ client/src/components/login-page/login-page.jsx
@@ -1,108 +1,106 @@
-import * as React from "react"
-import { Mutation } from "react-apollo"
-import { Link } from "react-router-dom"
-import gql from "graphql-tag"
++import * as React from "react"
++import { Mutation } from "react-apollo"
++import { Link } from "react-router-dom"
++import gql from "graphql-tag"
 import "./login-page.css"
 
-const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        id
-        name
-        email
-        username
-      }
-    }
-  }
-`
-
-class LoginPage extends React.Component {
-  state = {
-    email: "",
-    password: ""
-  }
-
-  onLogout = () => {
-    localStorage.removeItem("token")
-    window.location.replace('http://localhost:3001/');
-
-    // this.props.history.push("/login")
-  }
-
-
-  render() {
-    const token = localStorage.getItem("token")
-    return (
-      <div className="page">
-      
-
-        <Mutation mutation={LOGIN}>
-          {login => {
-            return (
-              <div className="login-form">
-                <form
-                  onSubmit={async e => {
-                    e.preventDefault()
-                    try {
-                      const { data } = await login({
-                        variables: {
-                          email: this.state.email,
-                          password: this.state.password
-                        }
-                      })
-                      localStorage.setItem("token", data.login.token)
-                      localStorage.setItem(
-                        "user",
-                        JSON.stringify(data.login.user)
-                      )
-                      this.props.history.push(`/${data.login.user.username}`)
-                    } catch (error) {
-                      localStorage.removeItem("token")
-                      localStorage.removeItem("user")
-                    }
-                  }}
-                >
-                  <div className="loginheading">
-                    <div className="heading">Log in to an existing account.</div>
-                    <div className="email"><input
-                      type="text"
-                      className="emailinput"
-                      placeholder="email"
-                      onChange={e => this.setState({ email: e.target.value })}
-                    />
-                    </div>
-                    <div className="passWord"><input
-                      type="password"
-                      className="passwordinput"
-                      placeholder="password"
-                      onChange={e =>
-                        this.setState({ password: e.target.value })
-                      }
-                    /></div>
-                    <div><button type="submit" className="loginbutton">
-                      submit
-                    </button></div><p/>
-                    <Link className="signuplink" to="/signup">
-                      Don't have an account? Sign Up!
-                    </Link>
-                    {/* {token ? (
-            <button onClick={this.onLogout} className="logout">logout</button>
-          ) : (
-            <Link to="/login" className="login">
-              login
-            </Link>
-          )} */}
-                  </div>
-                </form>
-              </div>
-            )
-          }}
-        </Mutation>
-      </div>
-    )
-  }
-}
-
-export default LoginPage
++
++const LOGIN = gql`
++  mutation login($email: String!, $password: String!) {
++    login(email: $email, password: $password) {
++      token
++      user {
++        id
++        name
++        email
++        username
++      }
++    }
++  }
++`
++
++class LoginPage extends React.Component {
++  state = {
++    email: "",
++    password: ""
++  }
++
++  onLogout = () => {
++    localStorage.removeItem("token")
++    window.location.replace('http://localhost:3001/');
++
++    // this.props.history.push("/login")
++  }
++
++
++  render() {
++    const token = localStorage.getItem("token")
++    return (
+<div>
++
++        <Mutation mutation={LOGIN}>
++          {login => {
++            return (
++              <div className="login-form">
++                <form
++                  onSubmit={async e => {
++                    e.preventDefault()
++                    try {
++                      const { data } = await login({
++                        variables: {
++                          email: this.state.email,
++                          password: this.state.password
++                        }
++                      })
++                      localStorage.setItem("token", data.login.token)
++                      localStorage.setItem(
++                        "user",
++                        JSON.stringify(data.login.user)
++                      )
++                      this.props.history.push(`/${data.login.user.username}`)
++                    } catch (error) {
++                      localStorage.removeItem("token")
++                      localStorage.removeItem("user")
++                    }
++                  }}
++                >
++                  <div className="loginheading">
++                    Log in to an existing account.
++                    <input
++                      type="text"
++                      className="emailinput"
++                      placeholder="Type email here"
++                      onChange={e => this.setState({ email: e.target.value })}
++                    />
++                    <input
++                      type="password"
++                      className="passwordinput"
++                      placeholder="Type password here"
++                      onChange={e =>
++                        this.setState({ password: e.target.value })
++                      }
++                    />
++                    <Link className="signuplink" to="/signup">
++                      Dont have an account? Sign Up!
++                    </Link>
++                    <button type="submit" className="loginbutton">
++                      submit
++                    </button>
++                    {token ? (
++            <button onClick={this.onLogout} className="logout">logout</button>
++          ) : (
++            <Link to="/login" className="login">
++              login
++            </Link>
++          )}
++                  </div>
++                </form>
++              </div>
++            )
++          }}
++        </Mutation>
++      </div>
++    )
++  }}
++
++export default LoginPage
