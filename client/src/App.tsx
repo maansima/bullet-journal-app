c13a28664321e593ceb04d82150ef9e89ac87037
Maansi Manchanda <maansima@usc.edu>
2018-07-18 13:20:47 +0100

    added in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { hot } from "react-hot-loader"
 import { Switch, Route } from "react-router-dom"
 import HomePage from "./components/home-page/home-page"
 import LoginPage from "./components/login-page/login-page"
+import Theme_One from "./components/theme-one/theme-one"
 import "./App.css"
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <Switch>
           <Route exact={true} path="/" component={HomePage} />
           <Route exact={true} path="/login" component={LoginPage} />
+          <Route exact={true} path="/theme-one" component={Theme_One} /> 
         </Switch>
       </div>
     )
